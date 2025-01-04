d3e3d30c6627d832bae9233bd9f073e5480bda83
Linar Aleev <aleevlr@yandex.ru>
2025-01-04 14:52:59 +0400

    added url literal constants to test_routes in unittest

--- ya_note/notes/tests/core.py
+++ ya_note/notes/tests/core.py
@@ -32,13 +32,30 @@ class TestBase(TestCase):
             slug=cls.USED_SLUG,
             author=cls.author
         )
-        cls.LOGIN_URL = reverse('users:login')
-        cls.LIST_URL = reverse('notes:list')
-        cls.ADD_URL = reverse('notes:add', args=None)
-        cls.EDIT_URL = reverse('notes:edit', kwargs={'slug': cls.note.slug})
-        cls.DELETE_URL = reverse('notes:delete', kwargs={
+        cls.LOGIN_URL_LITERAL = 'users:login'
+        cls.LOGOUT_URL_LITERAL = 'users:logout'
+        cls.SIGNUP_URL_LITERAL = 'users:signup'
+
+        cls.HOME_URL_LITERAL = 'notes:home'
+        cls.LIST_URL_LITERAL = 'notes:list'
+        cls.ADD_URL_LITERAL = 'notes:add'
+        cls.EDIT_URL_LITERAL = 'notes:edit'
+        cls.DELETE_URL_LITERAL = 'notes:delete'
+        cls.DETAIL_URL_LITERAL = 'notes:detail'
+        cls.DONE_URL_LITERAL = 'notes:success'
+
+        cls.LOGIN_URL = reverse(cls.LOGIN_URL_LITERAL)
+        cls.LOGOUT_URL = reverse(cls.LOGOUT_URL_LITERAL)
+        cls.SIGNUP_URL = reverse(cls.SIGNUP_URL_LITERAL)
+
+        cls.HOME_URL = reverse(cls.HOME_URL_LITERAL)
+        cls.LIST_URL = reverse(cls.LIST_URL_LITERAL)
+        cls.ADD_URL = reverse(cls.ADD_URL_LITERAL, args=None)
+        cls.EDIT_URL = reverse(cls.EDIT_URL_LITERAL, kwargs={
+                               'slug': cls.note.slug})
+        cls.DELETE_URL = reverse(cls.DELETE_URL_LITERAL, kwargs={
                                  'slug': cls.note.slug})
-        cls.DONE_URL = reverse('notes:success', args=None)
+        cls.DONE_URL = reverse(cls.DONE_URL_LITERAL, args=None)
 
         cls.form_data = {'title': cls.NOTE_TITLE,
                          'text': cls.NOTE_TEXT, 'slug': cls.NEW_SLUG}
--- ya_note/notes/tests/test_routes.py
+++ ya_note/notes/tests/test_routes.py
@@ -8,14 +8,14 @@ from .core import TestBase
 class TestRoutes(TestBase):
 
     def test_pages_availability(self):
-        """Проверка что анонимный пользователь можнт зайти на страницы: home,
+        """Проверка что анонимный пользователь может зайти на страницы: home,
         login, logout, signup.
         """
         urls = (
-            ('notes:home', None),
-            ('users:login', None),
-            ('users:logout', None),
-            ('users:signup', None),
+            (self.HOME_URL_LITERAL, None),
+            (self.LOGIN_URL_LITERAL, None),
+            (self.LOGOUT_URL_LITERAL, None),
+            (self.SIGNUP_URL_LITERAL, None),
         )
         for name, args in urls:
             with self.subTest(name=name):
@@ -36,7 +36,8 @@ class TestRoutes(TestBase):
 
             self.client.force_login(user)
 
-            for name in ('notes:edit', 'notes:delete', 'notes:detail'):
+            for name in (self.EDIT_URL_LITERAL, self.DELETE_URL_LITERAL,
+                         self.DETAIL_URL_LITERAL):
                 with self.subTest(user=user, name=name):
                     url = reverse(name, kwargs={'slug': self.note.slug})
                     response = self.client.get(url)
@@ -54,7 +55,8 @@ class TestRoutes(TestBase):
 
             self.client.force_login(user)
 
-            for name in ('notes:add', 'notes:list', 'notes:success'):
+            for name in (self.ADD_URL_LITERAL, self.LIST_URL_LITERAL,
+                         self.DONE_URL_LITERAL):
                 with self.subTest(user=user, name=name):
                     url = reverse(name, args=None)
                     response = self.client.get(url)
@@ -62,22 +64,22 @@ class TestRoutes(TestBase):
 
     def test_redirect_for_anonymous_client(self):
         """Проверка редиректа неавторизованного пользователя."""
-        login_url = reverse('users:login')
 
         for name in (
-            'notes:add',
-            'notes:list',
-            'notes:success',
-            'notes:edit',
-            'notes:detail',
-            'notes:delete',
+            self.ADD_URL_LITERAL,
+            self.LIST_URL_LITERAL,
+            self.DONE_URL_LITERAL,
+            self.EDIT_URL_LITERAL,
+            self.DETAIL_URL_LITERAL,
+            self.DELETE_URL_LITERAL,
         ):
             with self.subTest(name=name):
-                if name in ('notes:add', 'notes:list', 'notes:success',):
+                if name in (self.ADD_URL_LITERAL, self.LIST_URL_LITERAL,
+                            self.DONE_URL_LITERAL,):
                     url = reverse(name, args=None)
                 else:
                     url = reverse(name, kwargs={'slug': self.note.slug})
 
-                redirect_url = f'{login_url}?next={url}'
+                redirect_url = f'{self.LOGIN_URL}?next={url}'
                 response = self.client.get(url)
                 self.assertRedirects(response, redirect_url)
